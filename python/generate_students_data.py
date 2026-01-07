64895f5f8b75d8f8dbacbe498d3fc837686301f0
Val Marek <valmarek@vyncacare.com>
2026-01-07 13:31:41 -0800

    add comma

--- python/generate_students_data.py
+++ python/generate_students_data.py
@@ -4,7 +4,7 @@ from faker import Faker
 
 DB_NAME = os.environ.get("PGDATABASE", "school"),
 DB_USER = os.environ.get("PGUSER", "postgres"),
-DB_PASS = os.environ.get("PGPASSWORD", "")
+DB_PASS = os.environ.get("PGPASSWORD", ""),
 DB_HOST = os.environ.get("PGHOST", "localhost"),
 DB_PORT = os.environ.get("PGPORT", "5432")
 
