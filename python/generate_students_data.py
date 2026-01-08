a34044a0282ce4e3a58ff976e77343211366c28a
Val Marek <valmarek@vyncacare.com>
2026-01-08 09:41:16 -0800

    add datetime

--- python/generate_students_data.py
+++ python/generate_students_data.py
@@ -5,6 +5,7 @@ This script creates a database, adds a "students" table to it, and inserts 50 re
 import os
 import psycopg2
 from faker import Faker
+from datetime import date
 
 DB_NAME = os.environ.get("PGDATABASE", "school")
 DB_USER = os.environ.get("PGUSER", "postgres")
